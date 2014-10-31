a6a68263a3f4c66638e8a96afe32afcbfb219c1f
radicalsauce <krhale@gmail.com>
2014-10-31 16:14:30 -0700

    Created why template

--- website/client/app/src/components/Why.jsx
+++ website/client/app/src/components/Why.jsx
@@ -22,14 +22,22 @@ var WHY  = React.createClass({
     return (
       <div>
         <div className="container">
-          <h1 id="scrollWhy">WHY US REACT FULLSTACK?</h1>
-          <ul>
-            <li>Reason 1</li>
-            <li>Reason 2</li>
-            <li>Reason 3</li>
-          </ul>
-          <h3>Check out all this code!</h3>
-          <img src="./app/src/components/nickcage.jpg"/>
+         <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-4 col-xs-12">
+                  <h2>What this is:</h2>
+                    I think you've let your personal feelings cloud your judgement. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Travel time to the nearest starbase? Fate. It protects fools, little children, and ships named "Enterprise." Now we know what they mean by 'advanced' tactical training. We know you're dealing in stolen ore. Yes, absolutely, I do indeed concur, wholeheartedly!
+                </div>
+                <div className="col-md-4 col-xs-12">
+                  <h2>How it works:</h2>
+                    The game's not big enough unless it scares you a little. Your shields were failing, sir. We have a saboteur aboard. It's better than music. It's jazz. What's a knock-out like you doing in a computer-generated gin joint like this?  We know you're dealing in stolen ore. I'm afraid I still don't understand, sir. They were just sucked into space. Now we know what they mean by 'advanced' tactical training. Fear is the true enemy, the only enemy. Mr. Worf, you do remember how to fire phasers? 
+                </div>
+                <div className="col-md-4 col-xs-12">
+                  <h2>What this is:</h2>
+                    Shields up! Rrrrred alert! You bet I'm agitated! I may be surrounded by insanity, but I am not insane. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Mr. Crusher, ready a collision course with the Borg ship. Why don't we just give everybody a promotion and call it a night - 'Commander'? Well, I'll say this for him - he's sure of himself. A lot of things can change in twelve years, Admiral. Well, that's certainly good to know.
+                </div>
+              </div> 
+          </div>     
         </div>   
       </div>
       )
